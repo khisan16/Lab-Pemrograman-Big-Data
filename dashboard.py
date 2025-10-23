cf412b586f29acf0efac52b53cebf7ccbaf7f503
khisan16 <132869575+khisan16@users.noreply.github.com>
2025-10-23 23:16:17 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,6 @@ MODEL_PATH = "model/best.pt"
 def load_model():
     try:
         model = YOLO(MODEL_PATH)
-        st.success("Model berhasil dimuat ✅")
         return model
     except Exception as e:
         st.error(f"Gagal memuat model: {e}")
@@ -24,7 +23,7 @@ model = load_model()
 
 # Pilihan upload
 st.sidebar.header("Upload File")
-upload_type = st.sidebar.radio("Pilih jenis file:", ["Gambar", "Video"])
+upload_type = st.sidebar.radio("Pilih jenis file:", ["Gambar"])
 
 if upload_type == "Gambar":
     uploaded_file = st.file_uploader("Upload gambar...", type=["jpg", "jpeg", "png"])
@@ -39,23 +38,5 @@ if upload_type == "Gambar":
             res_plotted = results[0].plot()
 
         st.image(res_plotted, caption="Hasil Deteksi", use_container_width=True)
-
-elif upload_type == "Video":
-    uploaded_video = st.file_uploader("Upload video...", type=["mp4", "avi", "mov"])
-    if uploaded_video is not None and model:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
-            temp_video.write(uploaded_video.read())
-            st.video(temp_video.name)
-            st.info("Proses deteksi sedang berlangsung...")
-
-            # Jalankan deteksi
-            results = model(temp_video.name)
-            output_path = os.path.join("runs", "detect", "predict", "output.mp4")
-
-            # Cek hasil
-            if os.path.exists(output_path):
-                st.video(output_path)
-            else:
-                st.warning("Video hasil tidak ditemukan.")
 
 st.sidebar.info("Model YOLOv8 - Deteksi berbasis file best.pt")
